45f26401d264bb18b30373e156ddddd073dc0b32
M SRIHARSHA <amunknown5@gmail.com>
2019-04-29 17:34:07 +0530

    mongo atlas added

--- client/src/components/Dashbard/Dashboard.js
+++ client/src/components/Dashbard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../store/actions/login";
+import classes from './Dashboard.css'
 
 class Dashboard extends Component {
   onLogoutClick = e => {
@@ -13,26 +14,14 @@ class Dashboard extends Component {
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
+          <div className={classes.assigned}>
+            asd
+          </div>
+          <div className={classes.currentlyWorkingOn}>
+            dsa
+          </div>
+          <div className={classes.completed}>
+            sad
           </div>
         </div>
       </div>
@@ -46,7 +35,28 @@ Dashboard.propTypes = {
 const mapStateToProps = state => ({
   auth: state.login
 });
-export default connect(
-  mapStateToProps,
-  { logoutUser }
-)(Dashboard);
+export default connect(mapStateToProps, { logoutUser })(Dashboard);
+
+
+
+{/* <div className="col s12 center-align">
+  <h4>
+    <b>Hey there,</b> {user.name}
+    <p className="flow-text grey-text text-darken-1">
+      You are logged into a full-stack{" "}
+      <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+              </p>
+  </h4>
+  <button
+    style={{
+      width: "150px",
+      borderRadius: "3px",
+      letterSpacing: "1.5px",
+      marginTop: "1rem"
+    }}
+    onClick={this.onLogoutClick}
+    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+  >
+    Logout
+            </button>
+</div> */}
