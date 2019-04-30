71b6bbca32896a352a964f40b76f3a164ab0e72b
k-mayur <mayur.kumar@mountblue.io>
2019-04-30 19:11:50 +0530

    proxy address changed application working

--- client/src/store/actions/login.js
+++ client/src/store/actions/login.js
@@ -6,7 +6,7 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./actionTypes";
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("http://10.10.4.4:4000/api/users/register", userData)
+    .post("http://localhost:4000/api/users/register", userData)
     .then(res => history.push("/login")) // re-direct to login on successful register
     .catch(err =>
       dispatch({
