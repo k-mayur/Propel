4603c542c4598093ee71db86bbdb827d37a79d58
Pratip Basu <pratip.basu@mountblue.io>
2019-05-02 20:29:52 +0530

    mentor page modified

--- client/src/components/Mentor/index.js
+++ client/src/components/Mentor/index.js
@@ -8,15 +8,17 @@ import axios from "axios";
 import AddTask from "./addTaskForm";
 // import { error } from "util";
 
-// $(document).ready(function() {
-//   $('select').material_select();
-// });
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 export default class Todos extends Component {
   state = {
     todos: [],
     trainees: [],
+    options: [],
   };
+
+  options = ["option1", "option2", "option3"];
 
   componentDidMount = () => {
     axios.get("http://localhost:4000/api/tasks").then(res => {
@@ -25,6 +27,7 @@ export default class Todos extends Component {
     });
     // console.log(this.state.todos);
   };
+
   addTodo = todo => {
     // todo.id = Math.random();
     // let todos = [...this.state.todos, todo];
@@ -49,6 +52,23 @@ export default class Todos extends Component {
       .catch(err => console.log(err));
   };
 
+  deleteTodo = id => {
+    //   console.log(id);
+    let isDelete = confirm("Do you want to delete?");
+    if (isDelete) {
+      axios.delete(`http://localhost:4000/api/tasks/${id}`).then(res => {
+        this.state.todos = this.state.todos.filter(data => {
+          return data["_id"] !== id;
+        });
+        this.setState({});
+      });
+    }
+  };
+
+  handleChange = e => {
+    this.setState({})
+  };
+
   render() {
     const todos = this.state.todos;
     // const trainees = this.state.trainees;
@@ -58,27 +78,19 @@ export default class Todos extends Component {
         return (
           <div
             className="collection-item todoItem "
-            style={{margin: "50px"}}
+            style={{ margin: "50px" }}
             key={todo.id}
           >
             <span
               onClick={() => {
-                deleteTodo(todo["_id"]);
+                this.deleteTodo(todo["_id"]);
               }}
             >
               <h4 class="z-depth-5">{todo.task}</h4>
             </span>
             <h6>assigned to: </h6>
 
-            <div className="input-field col s12 ">
-              {/* <select multiple>
-                <option value="">
-                  choose trainee
-                </option>
-                {trainees.map(trainee => {
-                  return <option value={trainee.name}>{trainee.name}</option>;
-                })}
-              </select> */}
+            {/* <div className="input-field col s12 ">
               <a class="dropdown-trigger btn" href="#" data-target="dropdown1">
                 Choose trainee!
               </a>
@@ -104,26 +116,19 @@ export default class Todos extends Component {
                   </a>
                 </li>
               </ul>
-            </div>
+            </div> */}
+
+            <Select multiple value={this.options}>
+              {this.options.map(option => {
+                return <MenuItem>{option}</MenuItem>;
+              })}
+            </Select>
           </div>
         );
       })
     ) : (
       <p>No tasks yet</p>
     );
-
-    const deleteTodo = id => {
-      //   console.log(id);
-      let isDelete = confirm("Do you want to delete?");
-      if (isDelete) {
-        axios.delete(`http://localhost:4000/api/tasks/${id}`).then(res => {
-          this.state.todos = this.state.todos.filter(data => {
-            return data["_id"] !== id;
-          });
-          this.setState({});
-        });
-      }
-    };
 
     return (
       <div>
